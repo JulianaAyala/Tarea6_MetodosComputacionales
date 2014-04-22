b48b35aad2a8fc884d0b223d8a34999fa7e928e2
julianaayala <jm.ayala2908@uniandes.edu.co>
2014-04-22 10:52:06 -0500

    punto3

--- punto3/evolve.c
+++ punto3/evolve.c
@@ -1,12 +1,11 @@
 //
 //  evolve.c
-//  
+//
 //
 //  Created by Juliana Ayala and David Aleman
 //
-// codigo runge kutta desarrollado en 2013-2
 //
-
+//
 
 /*---------------------------------------------------------
  codigo que evoluciona un archivo con condiciones iniciales
@@ -15,16 +14,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#define FLOAT float
 
 //Definimos las constantes iguales a las de las condiciones iniciales
-const float PI= 3.14159;
-const float G= 4.296E-6; //Constante gravitacional en Km2 * kpc/(mSolares * s2)
-float h = 0.001;
+const float G=4.296E-6; //Constante gravitacional en Kpc3/(mSolares *kyear2)
 
-float func_prime_1(float x,float y_1,float x_1,float y_2,float *x_m,float *y_m,int *id,int a);
-float func_prime_2(float M,float x,float y_1,float x_1,float y_2,float *x_m ,float *y_m,int *id, int a);
-float *RungeKutta(float M,float x_old, float y1_old,float x_1, float y2_old,float *x_m,float *y_m,int *id,int a);
-
+void get_acceleration(FLOAT *ax, FLOAT *ay, FLOAT *az, FLOAT *x, FLOAT *y, FLOAT *z, FLOAT M, int N);
 
 /*----------Main-----------*/
 
@@ -33,10 +28,10 @@ int main(int argc, char **argv){
     //defino valores de entrada como masa, tiempo, momentos y el numero de particulas antes usado
     
     if(argc!=6){
-        printf("Se requieren 5 datos para introducir para inicializar; \n-Nombre del archivo:\n-Masa:\n-Tiempo:\n-Momentos:\n-Numero de datos(igual al usado antiguamente):\n");
+        printf("Se requieren 5 datos a introducir para inicializar; \n-Nombre del archivo:\n-Masa:\n-Tiempo:\n-Momentos:\n-Numero de datos(igual al usado antiguamente):\n");
         exit(1);
     }
-
+    
     
     float M=atof(argv[2]);
     float T=atof(argv[3]);
@@ -47,41 +42,66 @@ int main(int argc, char **argv){
     //llamo el archivo con las condiciones iniciales para evolucionar
     FILE *input;
     input=fopen(argv[1],"r");
-    
+
     /*---------------------------------------------------------
      llama el archivo producido, convierte y coloca en distintos arrays cada columna
      -----------------------------------------------------------*/
     float *x;
     float *y;
     float *z;
-    float *V_x;
-    float *V_y;
-    float *V_z;
+    float *v_x;
+    float *v_y;
+    float *v_z;
     int *ID;
     
     ID=malloc(num_lineas*sizeof(int));
     x= malloc(num_lineas*sizeof(float));
     y= malloc(num_lineas*sizeof(float));
     z= malloc(num_lineas*sizeof(float));
-    V_x= malloc(num_lineas*sizeof(float));
-    V_y= malloc(num_lineas*sizeof(float));
-    V_z= malloc(num_lineas*sizeof(float));
-    
+    v_x= malloc(num_lineas*sizeof(float));
+    v_y= malloc(num_lineas*sizeof(float));
+    v_z= malloc(num_lineas*sizeof(float));
+
     
     /*---------------------------------------------------------
      Registro de Datos en los arrays
      -----------------------------------------------------------*/
     rewind(input);
-    int i,j;
-    for(j=0;j<N;j++){
-        fscanf(input, "%d %f %f %f %f %f %f\n",&ID[j], &x[j],&y[j], &z[j],&V_x[j],&V_y[j], &V_z[j]);//en kpc, km/s
+    int j;
+    for(j=0;j<num_lineas;j++){
+        fscanf(input, "%d %f %f %f %f %f %f\n",&ID[j], &x[j],&y[j],&z[j],&v_x[j],&v_y[j], &v_z[j]);//en kpc, km/s
+        
+    /*-----------------------------------------------------------------------------------------
+        Crea grupos de archivos de salida
+    --------------------------------------------------------------------------------------*/
+        
+    int maximo=100000;
+    int p;
+    FILE *output1, *out;
+        
+    for (p=1;p<10;p++){
+            
+        char filename1[20];
+        sprintf(filename1,"Datos%d.txt",p);
+        
+        out=fopen(filename1,"r");
+        if (out){
+            fclose(out);
+        }
+        else{
+            output1=fopen(filename1,"a");
+            break;
+            
+        }
+    }
+
+
         
     /*---------------------------------------------------------
-     Usando RUNGEKUTTA:
-     se aplica a cada particula alrededor de la particula central el cambio de posicion y
-     velocidades dado por la interaccion con cada particula central presente por cada cambio de tiempo
-     -----------------------------------------------------------*/
-       
+        Usando RUNGEKUTTA:
+        se aplica a cada particula alrededor de la particula central el cambio de posicion y
+        velocidades dado por la interaccion con cada particula central presente por cada cambio de tiempo
+    -----------------------------------------------------------*/
     int part,centros;
     
     //Conteo de todas las particulas centrales
@@ -91,9 +111,10 @@ int main(int argc, char **argv){
             centros++;
         }
     }
-    /*-----------------------------------------------------------------------------------------
-     Crea una lista de la ubicacion y velocidades de los nuevos centros de masa
-     --------------------------------------------------------------------------------------*/
+        
+        /*-----------------------------------------------------------------------------------------
+         Crea una lista de la ubicacion y velocidades de los nuevos centros de masa
+         --------------------------------------------------------------------------------------*/
     int *CID;
     float *Cx;
     float *Cy;
@@ -101,211 +122,224 @@ int main(int argc, char **argv){
     float *CVx;
     float *CVy;
     float *CVz;
-    i=0;
-    
-    CID=malloc(centros*sizeof(int));
-    Cx=malloc(centros*sizeof(float));
-    Cy=malloc(centros*sizeof(float));
-    Cz=malloc(centros*sizeof(float));
-    CVx=malloc(centros*sizeof(float));
-    CVy=malloc(centros*sizeof(float));
-    CVz=malloc(centros*sizeof(float));
-    
+    int i=0;
+        
+    CID=malloc(num_lineas*sizeof(int));
+    Cx=malloc(num_lineas*sizeof(float));
+    Cy=malloc(num_lineas*sizeof(float));
+    Cz=malloc(num_lineas*sizeof(float));
+    CVx=malloc(num_lineas*sizeof(float));
+    CVy=malloc(num_lineas*sizeof(float));
+    CVz=malloc(num_lineas*sizeof(float));
+        
     for (j=-1;j<num_lineas;j++){
-        if( ID[j]==-1){
-            CID[i]=i;
+            CID[i]=j;
             Cx[i]=x[j];
             Cy[i]=y[j];
-            CVx[i]=V_x[j];
-            CVy[i]=V_y[j];
+            Cz[i]=z[j];
+            CVx[i]=v_x[j];
+            CVy[i]=v_y[j];
+            CVz[i]=v_z[j];
             i++;
-        }
+        fprintf(output1,"%f %f %f %f %f %f\n ", Cx[j], Cy[j], Cz[j], CVx[j], CVy[j], CVz[j]);
     }
-    
-    /*-----------------------------------------------------------------------------------------
-     Crea grupos de archivos de salida
-     --------------------------------------------------------------------------------------*/
-    
-    int maximo=100000;
-    int p;
-    FILE *output1, *out;
-    
-    for (p=1;p<5;p++){
         
-        char filename1[100];
-        sprintf(filename1,"Datos%d.txt",p);
+    /*terminos usados en la implementacion del metodo de Runge Kutta*/
+    
+    FLOAT *a2_x;
+    FLOAT *a2_y;
+    FLOAT *a2_z;
+    FLOAT *a3_x;
+    FLOAT *a3_y;
+    FLOAT *a3_z;
+    FLOAT *a4_x;
+    FLOAT *a4_y;
+    FLOAT *a4_z;
+    
+    FLOAT *dx1;
+    FLOAT *dy1;
+    FLOAT *dz1;
+    FLOAT *dx2;
+    FLOAT *dy2;
+    FLOAT *dz2;
+    FLOAT *dx3;
+    FLOAT *dy3;
+    FLOAT *dz3;
+    FLOAT *dx4;
+    FLOAT *dy4;
+    FLOAT *dz4;
+    
+    FLOAT *dv1_x;
+    FLOAT *dv1_y;
+    FLOAT *dv1_z;
+    FLOAT *dv2_x;
+    FLOAT *dv2_y;
+    FLOAT *dv2_z;
+    FLOAT *dv3_x;
+    FLOAT *dv3_y;
+    FLOAT *dv3_z;
+    FLOAT *dv4_x;
+    FLOAT *dv4_y;
+    FLOAT *dv4_z;
+    
+    FLOAT *x2;
+    FLOAT *y2;
+    FLOAT *z2;
+    
+    /*timestep variables*/
+    FLOAT h= 0.001;
+    int n_steps = (int) T/(100*(m-1));
+    
+    /*memory allocation*/
+    a2_x = malloc(num_lineas*sizeof(float));
+    a2_y = malloc(num_lineas*sizeof(float));
+    a2_z = malloc(num_lineas*sizeof(float));
+    a3_x = malloc(num_lineas*sizeof(float));
+    a3_y = malloc(num_lineas*sizeof(float));
+    a3_z = malloc(num_lineas*sizeof(float));
+    a4_x = malloc(num_lineas*sizeof(float));
+    a4_y = malloc(num_lineas*sizeof(float));
+    a4_z = malloc(num_lineas*sizeof(float));
+    dx1 = malloc(num_lineas*sizeof(float));
+    dy1 = malloc(num_lineas*sizeof(float));
+    dz1 = malloc(num_lineas*sizeof(float));
+    dx2 = malloc(num_lineas*sizeof(float));
+    dy2 = malloc(num_lineas*sizeof(float));
+    dz2 = malloc(num_lineas*sizeof(float));
+    dx3 = malloc(num_lineas*sizeof(float));
+    dy3 = malloc(num_lineas*sizeof(float));
+    dz3 = malloc(num_lineas*sizeof(float));
+    dx4 = malloc(num_lineas*sizeof(float));
+    dy4 = malloc(num_lineas*sizeof(float));
+    dz4 = malloc(num_lineas*sizeof(float));
+    dv1_x = malloc(num_lineas*sizeof(float));
+    dv1_y = malloc(num_lineas*sizeof(float));
+    dv1_z = malloc(num_lineas*sizeof(float));
+    dv2_x = malloc(num_lineas*sizeof(float));
+    dv2_y = malloc(num_lineas*sizeof(float));
+    dv2_z = malloc(num_lineas*sizeof(float));
+    dv3_x = malloc(num_lineas*sizeof(float));
+    dv3_y = malloc(num_lineas*sizeof(float));
+    dv3_z = malloc(num_lineas*sizeof(float));
+    dv4_x = malloc(num_lineas*sizeof(float));
+    dv4_y = malloc(num_lineas*sizeof(float));
+    dv4_z = malloc(num_lineas*sizeof(float));
+    x2 = malloc(num_lineas*sizeof(float));
+    y2 = malloc(num_lineas*sizeof(float));
+    z2 = malloc(num_lineas*sizeof(float));
+    
+    /*Aca se hace una implementacion del metodo de Runge Kutta de cuarto orden*/
+    
+    for(i=0;i<n_steps;i++){
         
+        get_acceleration(CVx, CVy, CVz, Cx, Cy, Cz, M, N);
         
-        out=fopen(filename1,"r");
-        if (out){
-            fclose(out);
-        }
-        else{
-            output1=fopen(filename1,"w");
-            break;
+        for(j=0;j<N;j++){
+            /*primer paso del Runge Kutta*/
+            dx1[j] = h*v_x[j];
+            dy1[j] = h*v_y[j];
+            dz1[j] = h*v_z[j];
+            
+            dv1_x[j] = h*CVx[j];
+            dv1_y[j] = h*CVy[j];
+            dv1_z[j] = h*CVz[j];
+            
+            x2[j] = x[j]+(dx1[j]/2.0);
+            y2[j] = y[j]+(dy1[j]/2.0);
+            z2[j] = z[j]+(dz1[j]/2.0);
             
         }
-    }
-    
-    
-    //Se corre el runge kutta para encontrar la evolucion de la posicion y las veloidades
         
-        float *data;
-        data=malloc(num_lineas*sizeof(float));
+        get_acceleration(a2_x,a2_y,a2_z,x2,y2,z2, M, N);
         
-        int time=0;
-        
-        //Avanza en el tiempo
-        while(time<200000){
-
-            //Por cada particula, avanza
-            for(part=0;part<num_lineas;part++){
-                if (time==100000) {
-                    for(part=0;part<num_lineas;part++){
-                        float lala1=V_x[part];
-                        float lala2=V_y[part];
-                        fprintf(output1,"%d %f %f %f %f\n",ID[part],x[part],y[part],lala1,lala2);
-                    }
-                }
-                
-                if(ID[part]==-1)
-                {
-                    if(centros==1){
-                        time+=h;
-                        x[part]+=(V_x[part]*h);
-                        y[part]+=(V_y[part]*h);
-                        
-                    }
-                    else{
-                        
-                        data=RungeKutta(M,time,x[part],y[part],V_x[part],Cx,Cy,ID,1);
-                        time+=h;
-                        x[part]=data[1];
-                        V_x[part]=data[2];
-                        data=RungeKutta(M,time,y[part],x[part],V_y[part],Cy,Cx,ID,1);
-                        y[part]=data[1];
-                        V_y[part]=data[2];
-                    }
-                }
-                
-                
-                else //(ID[part]>-1)
-                {
-                    data=RungeKutta(M,time,x[part],y[part],V_x[part],Cx,Cy,ID,0);
-                    x[part]=data[1];
-                    V_x[part]=data[2];
-                    data=RungeKutta(M,time,y[part],x[part],V_y[part],Cy,Cx,ID,0);
-                    y[part]=data[1];
-                    V_y[part]=data[2];
-                    time+=h;
-                }
-            }
-        }
-        
-    /*---------------------------------------------------------
-     exporta los datos de posicion y velocidades por cada particula en varios archivos que representan una ubicacion en el tiempo
-     -----------------------------------------------------------*/
-    
-    /*---------------------------------------------------------
-     FIN
-     -----------------------------------------------------------*/
-    return 0;
-    
-}
-}
-
-/*---------------------------------------------------------
- FUNCIONES
- -----------------------------------------------------------*/
-
-//Codigo define el metodo runge kutta para evolucionar los datos en evolve
-
-float func_prime_1(float x,float y_1,float x_1,float y_2,float *x_m,float *y_m,int *id,int a){
-    
-    return y_2;
-    
-}
-
-float func_prime_2(float M,float x,float y_1,float x_1,float y_2,float *x_m ,float *y_m,int *id, int a){
-    float chapa1,chapa2;
-    int ajiaco=sizeof(x_m);
-    if (a != 1){
-        
-        float deltax,deltay;
-        int i;
-        
-        for(i=0;i<ajiaco;i++){
+        for(j=0;j<N;j++){
+            /*segundo paso del runge Kutta*/
+            dx2[j] = h*(v_x[j]+(dv1_x[j]/2.0));
+            dy2[j] = h*(v_y[j]+(dv1_y[j]/2.0));
+            dz2[j] = h*(v_z[j]+(dv1_z[j]/2.0));
             
-            deltax=x_m[i]-y_1;
-            deltay=y_m[i]-x_1;
-            chapa1=pow(deltax,2)+pow(deltay,2);
-            chapa2=-(G*M/pow(chapa1,(3/2)))*deltax;
+            dv2_x[j] = h*a2_x[j];
+            dv2_y[j] = h*a2_y[j];
+            dv2_z[j] = h*a2_z[j];
+            
+            x2[j] = x[j]+(dx2[j]/2.0);
+            y2[j] = y[j]+(dy2[j]/2.0);
+            z2[j] = z[j]+(dz2[j]/2.0);
+            
         }
-    }
-    else{
-        int i;
         
-        for(i=0;i<ajiaco;i++){
-            if(i==id[i]){
-                continue;
-            }
-            float deltax=x_m[i]-y_1;
-            float deltay=y_m[i]-x_1;
-            chapa1=pow(deltax,2)+pow(deltay,2);
-            chapa2=-(G*M/pow(chapa1,(3/2)))*deltax;
+        get_acceleration(a3_x,a3_y,a3_z,x2,y2,z2, M, N);
+        
+        for(j=0;j<N;j++){
+            /*tercer paso del Runge Kutta*/
+            dx3[j] = h*(v_x[j]+(dv2_x[j]/2.0));
+            dy3[j] = h*(v_y[j]+(dv2_y[j]/2.0));
+            dz3[j] = h*(v_z[j]+(dv2_z[j]/2.0));
+            
+            dv3_x[j] = h*a3_x[j];
+            dv3_y[j] = h*a3_y[j];
+            dv3_z[j] = h*a3_z[j];
+            
+            x2[j] = x[j]+dx3[j];
+            y2[j] = y[j]+dy3[j];
+            z2[j] = z[j]+dz3[j];
+            
         }
+        
+        get_acceleration(a4_x,a4_y,a4_z,x2,y2,z2, M, N);
+        
+        for(j=0;j<N;j++){
+            /*cuarto paso del Runge Kutta*/
+            dx4[j] = h*(v_x[j]+dv3_x[j]);
+            dy4[j] = h*(v_y[j]+dv3_y[j]);
+            dz4[j] = h*(v_z[j]+dv3_z[j]);
+            
+            dv4_x[j] = h*a4_x[j];
+            dv4_y[j] = h*a4_y[j];
+            dv4_z[j] = h*a4_z[j];
+            
+            
+        }
+        
+        for(j=0;j<N;j++){
+            /*Donde las aproximaciones para las velocidades y posiciones en cada instante de tiempo vendran dadas por: */
+            
+            x[j] = x[j] + (1.0/6.0)*(dx1[j]+(2*dx2[j])+(2*dx3[j])+dx4[j]);
+            y[j] = y[j] + (1.0/6.0)*(dy1[j]+(2*dy2[j])+(2*dy3[j])+dy4[j]);
+            z[j] = z[j] + (1.0/6.0)*(dz1[j]+(2*dz2[j])+(2*dz3[j])+dz4[j]);
+            
+            v_x[j] = v_x[j] + (1.0/6.0)*(dv1_x[j]+(2*dv2_x[j])+(2*dv3_x[j])+dv4_x[j]);
+            v_y[j] = v_y[j] + (1.0/6.0)*(dv1_y[j]+(2*dv2_y[j])+(2*dv3_y[j])+dv4_y[j]);
+            v_z[j] = v_z[j] + (1.0/6.0)*(dv1_z[j]+(2*dv2_z[j])+(2*dv3_z[j])+dv4_z[j]);
+            fprintf(output1," %f %f %f %f %f %f\n ", x[j], y[j], z[j], v_x[j], v_y[j], v_z[j]);
+        }
+        
+        
     }
-    return chapa2;
+    fclose(output1);
+
+}
 }
 
-
-float *RungeKutta(float M,float x_old, float y1_old,float x_1, float y2_old,float *x_m,float *y_m,int *id,int a){
-
-    float k1= func_prime_1(x_old,y1_old,x_1,y2_old, x_m,y_m,id,a);
-    float k11= func_prime_2(M,x_old,y1_old,x_1,y2_old, x_m, y_m,id,a);
-    //Primer paso
-    
-    float x1=x_old + (h/2.0);
-    float y1=y1_old + (h/2.0)*k1;
-    float y11=y2_old + (h/2.0)*k11;
-    
-    float k2= func_prime_1(x1,y1,x_1,y11,x_m, y_m,id,a);
-    float k21= func_prime_2(M,x1,y1,x_1,y11,x_m, y_m,id,a);
-    
-    //segundo paso
-    
-    float x2=x_old + (h/2.0);
-    float y2=y1_old + (h/2.0)*k2;
-    float y21=y2_old + (h/2.0)*k2;
-    
-    float k3= func_prime_1(x2,y2,x_1,y21,x_m, y_m,id,a);
-    float k31= func_prime_2(M,x2,y2,x_1,y21,x_m, y_m,id,a);
-    
-    
-    //tercer paso
-    
-    float x3=x_old + (h);
-    float y3=y1_old + (h)*k3;
-    float y31=y2_old + (h)*k31;
-    
-    float k4= func_prime_1(x3,y3,x_1,y31,x_m, y_m,id,a);
-    float k41= func_prime_2(M,x3,y3,x_1,y31, x_m, y_m,id,a);
-    
-    //cuarto paso
-    float prom_k1=(1.0/6.0)*(k1 + 2.0*k2+ 2.0*k3 + k4);
-    float prom_k2=(1.0/6.0)*(k11 + 2.0*k21+ 2.0*k31 + k41);
-    
-    
-    
-    
-    //entrega de datos
-    float x_new =x_old + h;
-    float y1_new=y1_old + h*prom_k1;
-    float y2_new=y2_old + h*prom_k2;
-    float *mojo = malloc(3*sizeof(float));
-    mojo[0]=x_new;
-    mojo[1]=y1_new;
-    mojo[2]=y2_new;
-    return mojo;
-    
+void get_acceleration(FLOAT *ax, FLOAT *ay, FLOAT *az, FLOAT *x, FLOAT *y, FLOAT *z, FLOAT M, int N){
+    int i,j;
+    FLOAT r_ij;
+    for(i=0;i<N;i++){
+        ax[i]=0.0;
+        ay[i]=0.0;
+        az[i]=0.0;
+        
+        for(j=0;j<N;j++){
+            if(j!=i){
+                r_ij = (pow((x[i] - x[j]),2.0) + pow((y[i] - y[j]),2.0) + pow((z[i] - z[j]),2.0));
+                r_ij = sqrt(r_ij);
+                ax[i] += -G *M/ pow(r_ij,1.5) * (x[i] - x[j]);
+                ay[i] += -G *M/ pow(r_ij,1.5) * (y[i] - y[j]);
+                az[i] += -G *M/ pow(r_ij,1.5) * (z[i] - z[j]);
+                
+            }
+        }    
+    }  
 }
+
+    
